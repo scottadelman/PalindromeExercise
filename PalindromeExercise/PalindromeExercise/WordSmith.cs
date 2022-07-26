82252e6794381ff8231285a1928693ee84ba6021
Scott <ScottAdelman97@gmail.com>
2022-07-26 08:43:37 -0600

    added tests

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PalindromeExercise;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+
+        }
+        public bool IsAPalindrome(string word)
+        {
+           return word.ToLower().SequenceEqual(word.ToLower().Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,22 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Hannah", true)]
+        [InlineData("Train", false)]
         public void Test1()
         {
+            var wordSmith = new WordSmith();
+
+            var actual = wordSmith.IsAPalindrome(word);
+
+            Assert.Equal(true, actual);
 
         }
     }
